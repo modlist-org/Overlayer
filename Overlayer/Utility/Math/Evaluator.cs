ce7fc15a120a061d862fb0a9f69233bacebead23
Kkitut <digitalhdmiport@gmail.com>
2026-08-06 13:17:28 +0900

    update NCalc, Optimize Evaluator, Rename EvalState.OK -> EvalState.Ok

--- Overlayer/Utility/Math/Evaluator.cs
+++ Overlayer/Utility/Math/Evaluator.cs
@@ -1,52 +1,72 @@
 ﻿using NCalc;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Overlayer.Utility.Math;
 
 public enum EvalState {
-    OK,
+    Ok,
     Error,
     Same,
     OverRange,
     UnderRange
 }
 
-public static class Evaluator<T> where T : struct, IComparable, IConvertible {
-    private static readonly Dictionary<string, double> Constants = new() {
+public static class EvaluatorConstants {
+    public static readonly Dictionary<string, double> Constants = new() {
         { "PI", System.Math.PI },
         { "E", System.Math.E }
     };
 
+    public const NumberStyles NumStyle = NumberStyles.Float | NumberStyles.AllowThousands;
+    public static readonly CultureInfo Culture = CultureInfo.InvariantCulture;
+}
+
+public static class Evaluator<T> where T : struct, IComparable<T>, IConvertible {
+
     public static (T result, EvalState state) Evaluate(string exprStr, T currentVal, T? min = null, T? max = null) {
-        if(string.IsNullOrWhiteSpace(exprStr)) {
+        if (string.IsNullOrWhiteSpace(exprStr)) {
             return (currentVal, EvalState.Error);
+        }
+
+        if (double.TryParse(exprStr, EvaluatorConstants.NumStyle, EvaluatorConstants.Culture, out double parsedDirect)) {
+            T directResult = (T)Convert.ChangeType(parsedDirect, typeof(T), EvaluatorConstants.Culture);
+            return ValidateAndReturn(directResult, currentVal, min, max);
         }
 
         try {
             var e = new Expression(exprStr, ExpressionOptions.IgnoreCaseAtBuiltInFunctions);
-            foreach(var constant in Constants) {
+
+            foreach (var constant in EvaluatorConstants.Constants) {
                 e.Parameters[constant.Key] = constant.Value;
             }
 
             object evalResult = e.Evaluate();
-            T result = (T)Convert.ChangeType(evalResult, typeof(T));
-
-            if(min.HasValue && max.HasValue) {
-                if(result.CompareTo(min.Value) < 0) {
-                    return (min.Value, EvalState.UnderRange);
-                }
-
-                if(result.CompareTo(max.Value) > 0) {
-                    return (max.Value, EvalState.OverRange);
-                }
+            if (evalResult == null) {
+                return (currentVal, EvalState.Error);
             }
 
-            if(result.CompareTo(currentVal) == 0) {
-                return (result, EvalState.Same);
-            }
-
-            return (result, EvalState.OK);
+            T result = (T)((IConvertible)evalResult).ToType(typeof(T), EvaluatorConstants.Culture);
+            return ValidateAndReturn(result, currentVal, min, max);
         } catch {
             return (currentVal, EvalState.Error);
         }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static (T result, EvalState state) ValidateAndReturn(T result, T currentVal, T? min, T? max) {
+        if (min.HasValue && result.CompareTo(min.Value) < 0) {
+            return (min.Value, EvalState.UnderRange);
+        }
+
+        if (max.HasValue && result.CompareTo(max.Value) > 0) {
+            return (max.Value, EvalState.OverRange);
+        }
+
+        if (EqualityComparer<T>.Default.Equals(result, currentVal)) {
+            return (result, EvalState.Same);
+        }
+
+        return (result, EvalState.Ok);
     }
 }
--- Overlayer/Utility/Math/MathVisuals.cs
+++ Overlayer/Utility/Math/MathVisuals.cs
@@ -5,7 +5,7 @@ namespace Overlayer.Utility.Math;
 
 public static class MathVisuals {
     public static Color GetStateColor(EvalState state) => state switch {
-        EvalState.OK => UIColors.ObjectActiveMathOk,
+        EvalState.Ok => UIColors.ObjectActiveMathOk,
         EvalState.Error => UIColors.ObjectActiveMathErr,
         EvalState.Same => UIColors.ObjectActive,
         EvalState.OverRange => UIColors.ObjectActiveMathWarn,
