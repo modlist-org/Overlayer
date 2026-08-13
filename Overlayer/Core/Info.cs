281fac04560ea78e06c8709d1ee704442c5b63ae
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-08-13 19:55:56 +0900

    5.0.2

--- Overlayer/Core/Info.cs
+++ Overlayer/Core/Info.cs
@@ -3,7 +3,7 @@
 public static class Info {
     public const string Name = "Overlayer";
     public const string Author = "modlist.org";
-    public const string Version = "5.0.1";
+    public const string Version = "5.0.2";
     public const string Description = "Custom UI Overlay Mod for Unity Games";
     public const string GithubLink = "https://github.com/modlist-org/Overlayer-v5";
 }
